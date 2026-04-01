71a128c844e2fd718e1e3a52b9b12e909f29c8cb
Genglin Guo <2019207025@njau.edu.cn>
2026-04-01 19:36:12 -0400

    modified:   src/leptyper/database/mlst_scheme_1/download.sh

--- src/leptyper/database/mlst_scheme_1/download.sh
+++ src/leptyper/database/mlst_scheme_1/download.sh
@@ -1,12 +1,14 @@
+#!/bin/bash
+
 # profiles
 curl https://rest.pubmlst.org/db/pubmlst_leptospira_seqdef/schemes/1/profiles_csv \
--o profiles.tsv
-
+ -o profiles.tsv
+ 
 # loci
 for locus in glmU_1 pntA_1 sucA_1 tpiA_1 pfkB_1 mreA_1 caiB_1
 do
     curl "https://rest.pubmlst.org/db/pubmlst_leptospira_seqdef/loci/${locus}/alleles_fasta" \
-    -o ${locus}.fasta
+     -o ${locus}.fasta
 done
 
 # convert fasta to one line
--- src/leptyper/database/mlst_scheme_2/download.sh
+++ src/leptyper/database/mlst_scheme_2/download.sh
@@ -1,12 +1,14 @@
-# profiles
-curl https://rest.pubmlst.org/db/pubmlst_leptospira_seqdef/schemes/2/profiles_csv \
--o profiles.tsv
+#!/bin/bash
 
+# profiles
+curl https://rest.pubmlst.org/db/pubmlst_leptospira_seqdef/schemes/1/profiles_csv \
+ -o profiles.tsv
+ 
 # loci
 for locus in adk_2 glmU_2 icdA_2 lipL32_2 lipL41_2 mreA_2 pntA_2
 do
     curl "https://rest.pubmlst.org/db/pubmlst_leptospira_seqdef/loci/${locus}/alleles_fasta" \
-    -o ${locus}.fasta
+     -o ${locus}.fasta
 done
 
 # convert fasta to one line
